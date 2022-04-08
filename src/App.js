906062a6b87bf727f1eefefa22df45d730933e41
Gukss <dongkuk97@naver.com>
2022-04-08 18:03:16 +0900

    main layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrap">
+				<header id="header">
+
+				</header>
+				{/* header */}
+				<section id="section">
+					<div className='board'></div>
+					<div className='board'></div>
+					<div className='board'></div>
+					<div className='board'></div>
+					<div className='board'></div>
+				</section>
+				{/* section */}
+				<footer id="footer">
+
+				</footer>
+				{/* footer */}
+			</div>
+			{/* wrap */}
     </div>
+		// App
   );
 }
 
