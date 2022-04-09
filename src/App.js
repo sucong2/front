74524ac08237eaeff69e7cace9497ed6f9b4fb30
Gukss <dongkuk97@naver.com>
2022-04-09 18:22:56 +0900

    header;location select add search, down arrow icon

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ function App() {
   return (
     <div className="App">
       <div id="wrap">
-				<header id="header">
-
-				</header>
-				{/* header */}
-				<section id="section">
-					<div className='board'></div>
-					<div className='board'></div>
-					<div className='board'></div>
-					<div className='board'></div>
-					<div className='board'></div>
-				</section>
-				{/* section */}
-				<footer id="footer">
-
-				</footer>
-				{/* footer */}
-			</div>
-			{/* wrap */}
+        <header id="header">
+          <div className="header_container">
+						<form>
+							<select className='location' size='1'>
+								<option>전체</option>
+								<option>푸름관</option>
+								<option>오름관</option>
+								<option>아름관</option>
+								<option>블랙홀</option>
+							</select>
+						</form>
+          </div>
+        </header>
+        {/* header */}
+        <section id="section">
+          <div className="board"></div>
+          <div className="board"></div>
+          <div className="board"></div>
+          <div className="board"></div>
+          <div className="board"></div>
+        </section>
+        {/* section */}
+        <footer id="footer"></footer>
+        {/* footer */}
+      </div>
+      {/* wrap */}
     </div>
-		// App
+    // App
   );
 }
 
