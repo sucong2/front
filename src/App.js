562e26952bc4ff931134ba91a475960150939724
김수빈 <win3137054@gmail.com>
2022-05-03 21:03:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 /* Page */
 import Home from './Page/HomePage/Home';
 import Board from './Page/BoardPage/Board';
+import MyPage from '/Page/MyPage/mypage';
 
 function App() {
   return (
